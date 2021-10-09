3dd15f2600c7c6412db51de87e6b38f222a966cb
Alex Kong <alexkong0222@gmail.com>
2021-10-09 16:22:22 +0800

    done for the prefix tree structure

--- prefixTree.py
+++ prefixTree.py
@@ -1,32 +1,79 @@
-class Trie(object):
+# Structure of Prefix Tree
+
+# TrieNode: null char is added into the text when init, and contains a dict for storing all its children
+class TrieNode:
+    def __init__(self, text=''):
+        self.text = text
+        self.children = dict()
+        ''' dict={character of the child: address of the child Trienode} '''
+        self.is_word = False
+        '''Boolean indicating if the words together in front of the TrieNode is a single word'''
+
+
+# Prefix Tree is built with a root node which is a null node when init
+class PrefixTree:
     def __init__(self):
-        self.child = {}
+        self.root = TrieNode()
+
+    # Operations of Prefix Tree
+
+    # Inserting a new word to a PrefixTree
 
     def insert(self, word):
-        current = self.child
-        for l in word:
-            if l not in current:
-                current[l] = {}
-            current = current[l]
-        current['#'] = 1
+        current = self.root
+        for i, char in enumerate(word):
+            if char not in current.children:
+                prefix = word[0:i+1]
+                current.children[char] = TrieNode(prefix)
+            current = current.children[char]
+        current.is_word = True
 
-    def search(self, word):
-        current = self.child
-        for l in word:
-            if l not in current:
-                return False
-            current = current[l]
-        return '#' in current
+    # Returning the TrieNode representing the given word
 
-    def startsWith(self, prefix):
-        current = self.child
-        for l in prefix:
-            if l not in current:
-                return False
-            current = current[l]
-        return True
+    def find(self, word):
+        '''
+        Returns the TrieNode representing the given word if it exists
+        and None otherwise.
+        '''
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return None
+            current = current.children[char]
+
+        if current.is_word:
+            return current
+
+    # Returning a list of all words beginning with the given prefix, or an empty list if no words begin with that prefix
+    def starts_with(self, prefix):
+        words = list()
+        current = self.root
+        for char in prefix:
+            if char not in current.children:  # if that prefix are not appeared in the Tree, return empty list
+                return list()
+            current = current.children[char]
+
+        self.__child_words_for(current, words)
+        return words
+
+    # Private helper function. Cycles through all children of node recursively, adding them to words if they constitute whole words
+    def __child_words_for(self, node, words):
+        if node.is_word:
+            words.append(node.text)
+        for letter in node.children:
+            self.__child_words_for(node.children[letter], words)
 
 
-ob1 = Trie()
-ob1.insert("apple")
-print(ob1)
+# Implementation
+if __name__ == '__main__':
+    trie = PrefixTree()
+    trie.insert('apple')
+    trie.insert('app')
+    trie.insert('aposematic')
+    trie.insert('appreciate')
+    trie.insert('book')
+    trie.insert('bad')
+    trie.insert('bear')
+    trie.insert('bat')
+
+    print(trie.starts_with('a'))
