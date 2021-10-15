bd67dd6816c519a856c1e857ca1c4f542a0b8ba5
tkhong9 <1155125453@link.cuhk.edu.hk>
2021-10-15 14:00:03 +0800

    Add the merge function

--- seperate.py
+++ seperate.py
@@ -2,11 +2,10 @@ from extract import extract
 from DictionaryPrefixTree import DictionaryPrefixTree
 from PrefixTree import PrefixTree
 import os
+import math
 
 # Label data folder path
-#path = "labeled_data"
 path = os.path.join(os.path.dirname(__file__), "labeled_data")
-
 
 # Seperate the sentance as a prefix tree structure and return a list.
 def seperate(self, sentance):
@@ -25,6 +24,23 @@ def seperate(self, sentance):
                 sentance_list.append(str3)
     return sentance_list
 
+def merge(dictionary, len):
+    MI_value = {}
+    merged_word = []
+    threshold = 5 # By experiment, set threshold = 5 first.
+    for key1 in dictionary:
+        for key2 in dictionary:
+            if key1 + key2 in dictionary:
+                merge = key1 + key2
+                count1 = dictionary[key1]
+                count2 = dictionary[key2]
+                merge_count = dictionary[merge]
+                print(merge)
+                total = (merge_count * len) / (count1 * count2)
+                MI_value[merge] = math.log(total, 2)
+                if MI_value[merge] >= threshold:
+                    merged_word.append(merge)
+    return merged_word
 
 ### Example (print out frequency and vocabs of given sentence)  ###
 if __name__ == '__main__':
@@ -35,12 +51,13 @@ if __name__ == '__main__':
     # Sentence would like to analysis
     # sentence = "廣東話容唔容易學"
 
-    sentence = "我係一個西伯利亞人"
+    sentence = "我好中意食麥當勞"
 
     sentence1 = "世一中場佐真奴"
     sentence2 = "今天我寒夜裡看雪飄過"
     sentence3 = ""
     sentence4 = "我係一個西伯利亞人"
+    sentence5 = "中國嘅伊斯蘭教會"
 
     # Search it from the dictionary
     dict_trie = DictionaryPrefixTree()
@@ -58,7 +75,8 @@ if __name__ == '__main__':
 
     ####### Output1: Print out all vocab in vocab_dictionary which contains in sentence #######
     print("Vocabs contains:")
-    print(dict_trie.contains(sentence))
+    vocabs = dict_trie.contains(sentence)
+    print(vocabs)
     print()
     # Separate the sentance and search it from the label data
     sentance_list = trie.seperate(sentence)
@@ -80,6 +98,8 @@ if __name__ == '__main__':
         count_char = 0
     print("frequency:")
     print(dict)
-    print(label_data_len)
+    print("Label Data Length:", label_data_len)
+    #dict2 = {'中': 170, '中國': 20, '斯': 300, '國': 144, '教會': 2, '蘭': 200, '嘅': 1606, '教': 96, '會': 830}
+    print(merge(dict, label_data_len))
 
 
