049feef44471faf29ef6cf6530e89504927486c8
tkhong9 <1155125453@link.cuhk.edu.hk>
2021-10-29 09:58:02 +0800

    Add the testing_data folder

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fscore import f_score
 from segment import segment
 
-
 # Input a list of unsegmented sentence,
 # output the segmentation result,calculating the F-score
 # (Now unable the merging measure)
@@ -10,7 +9,7 @@ if __name__ == '__main__':
 
     sentence1 = "世一中場佐真奴"
     sentence2 = "大學生活多姿多彩"
-    sentence3 = "你嗰陣時有冇諗過冇人想知" # Now is "嗰陣/時", we should change to "嗰陣時" by considering label data
+    sentence3 = "你嗰陣時有冇諗過放棄" # Now is "嗰陣/時", we should change to "嗰陣時" by considering label data
     sentence4 = "廣東話容唔容易學"
     sentence5 = "大家都好中意食麥當勞"
     sentence6 = "我老闆想我今晚十點之前做完啲嘢"
@@ -23,39 +22,35 @@ if __name__ == '__main__':
     sentence11 = "大學生活多姿多彩"
 
     ### Sentence would like to analysis ###
-    sentence = sentence11
     sentence_list = []
-    #sentence_list.append(sentence1)
-    #sentence_list.append(sentence2)
-    sentence_list.append(sentence3)
-    #sentence_list.append(sentence4)
-    #sentence_list.append(sentence5)
-    #sentence_list.append(sentence6)
-    sentence_list.append(sentence7)
-    sentence_list.append(sentence8)
+    sentence_list2 = []
+    with open("testing_data/testing_data.txt", 'r', encoding='utf-8') as fd:
+        sentence_list = fd.readlines()
+    for i in range(len(sentence_list)):
+        tmp = sentence_list[i].replace("\n", "")
+        sentence_list2.append(tmp)
 
-
-    segmentation_result = segment(sentence_list)
+    segmentation_result = segment(sentence_list2)
 
     # Print the segmentation result
-    print("\n")
     print("segmentation_result_list: "+str(segmentation_result))
-    print("\n")
 
     '''
     correct_sentence_list = [
         '世一/中場/佐真奴',
         '大學/生活/多姿多彩',
-        '你/嗰陣時/有冇/諗過/冇人/想/知',
+        '你/嗰陣時/有冇/諗過/放棄',
         '廣東話/容/唔/容易/學',
         '大家/都/好/中意/食/麥當勞',
         '我/老闆/想/我/今晚/十點/之前/做完/啲/嘢']
     '''
-
-    correct_sentence_list = [
-        '你/嗰陣時/有冇/諗過/冇人/想/知', 
-        '中國/伊斯蘭教/嘅/伊斯蘭/教會',
-        '我/係/大學生/幹事會/嘅/成員']
+    correct_sentence_list = []
+    correct_sentence_list2 = []
+    with open("testing_data/correct_data.txt", 'r', encoding='utf-8') as fd:
+        correct_sentence_list = fd.readlines()
+    for j in range(len(correct_sentence_list)):
+        tmp = correct_sentence_list[j].replace("\n", "")
+        correct_sentence_list2.append(tmp)
 
     # Calculate Precision
-    f_score(segmentation_result, correct_sentence_list)
+    f_score(segmentation_result, correct_sentence_list2)
